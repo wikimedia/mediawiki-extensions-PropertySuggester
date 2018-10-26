e96c0aba64bfe1d29adfac32e823833cacf12153
Umherirrender <umherirrender_de.wp@web.de>
2018-10-26 21:27:48 +0200

    Start showing warning for deprecated PHP entry point
    Change-Id: I2215f3fbfdb74691d651a0fc7f83d2def9eace57

--- PropertySuggester.php
+++ PropertySuggester.php
@@ -9,11 +9,11 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['PropertySuggester'] = __DIR__ . '/i18n';
 	$wgExtensionMessagesFiles['PropertySuggesterAlias'] = __DIR__ . '/PropertySuggester.alias.php';
-	/*wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for PropertySuggester extension. Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	);*/
+	);
 	return;
 } else {
-	die( 'This version of the PropertySuggester extension requires MediaWiki 1.25+' );
+	die( 'This version of the PropertySuggester extension requires MediaWiki 1.31+' );
 }
