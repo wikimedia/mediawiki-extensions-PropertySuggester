47b35197df00e40bf5fd05488669681a6a72f253
Umherirrender <umherirrender_de.wp@web.de>
2024-12-08 22:12:02 +0100

    Add @inheritDoc to functions
    Add @inheritDoc to functions found by the FunctionComment
    sniff to remove the exclusion of the sniff.
    The activated sniff avoids that new code is missing doc comments or
    real-typehints for arguments and return value.
    
    Change-Id: Ibea039ceba0dd8b239ef1ac088599a9c2c565a5e

--- src/SchemaHooks.php
+++ src/SchemaHooks.php
@@ -8,6 +8,7 @@ use MediaWiki\Installer\Hook\LoadExtensionSchemaUpdatesHook;
  * @license GPL-2.0-or-later
  */
 final class SchemaHooks implements LoadExtensionSchemaUpdatesHook {
+	/** @inheritDoc */
 	public function onLoadExtensionSchemaUpdates( $updater ) {
 		$updater->addExtensionTable(
 			'wbs_propertypairs',
