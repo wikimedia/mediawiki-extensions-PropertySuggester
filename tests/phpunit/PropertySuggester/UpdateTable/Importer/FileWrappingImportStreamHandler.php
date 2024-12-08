c3bb1d37aeabc44a223bca7412cf95c9dc236e39
Umherirrender <umherirrender_de.wp@web.de>
2024-12-08 22:11:05 +0100

    tests: Add function documentation to FileWrappingImportStreamHandler
    Use types from https://www.php.net/manual/en/class.streamwrapper.php
    Does not use type-declaration as CI gets a Segmentation fault
    
    Move the phpcs ignore to the outer scope as seen in other classes
    
    Change-Id: I5916880da2d6102f71d8b9de32603bd0756f2012

--- tests/phpunit/PropertySuggester/UpdateTable/Importer/FileWrappingImportStreamHandler.php
+++ tests/phpunit/PropertySuggester/UpdateTable/Importer/FileWrappingImportStreamHandler.php
@@ -4,6 +4,11 @@ declare( strict_types = 1 );
 
 namespace PropertySuggester\Tests\PropertySuggester\UpdateTable\Importer;
 
+// phpcs:disable MediaWiki.NamingConventions.LowerCamelFunctionsName.FunctionName
+
+/**
+ * The class is registered via stream_wrapper_register
+ */
 class FileWrappingImportStreamHandler {
 
 	/** @var resource|null Must exist on stream wrapper class */
@@ -11,21 +16,40 @@ class FileWrappingImportStreamHandler {
 	/** @var resource|null The opened file */
 	protected $filehandle;
 
-	// phpcs:disable MediaWiki.NamingConventions.LowerCamelFunctionsName.FunctionName
+	/**
+	 * @param string $path
+	 * @param string $mode
+	 * @param int $options
+	 * @param ?string &$opened_path
+	 * @return bool
+	 */
 	public function stream_open( $path, $mode, $options, &$opened_path ): bool {
 		$filepath = substr( $path, strlen( "testimport://" ) );
 		$this->filehandle = fopen( $filepath, $mode, false );
 		return true;
 	}
 
+	/**
+	 * @param int $bytes
+	 * @return string|false
+	 */
 	public function stream_read( $bytes ) {
 		return fread( $this->filehandle, $bytes );
 	}
 
+	/**
+	 * @param string $data
+	 * @return int
+	 */
 	public function stream_write( $data ): int {
 		return 0;
 	}
 
+	/**
+	 * @param int $offset
+	 * @param int $whence
+	 * @return bool
+	 */
 	public function stream_seek( $offset, $whence ): bool {
 		return false;
 	}
@@ -34,6 +58,9 @@ class FileWrappingImportStreamHandler {
 		return feof( $this->filehandle );
 	}
 
+	/**
+	 * @return int
+	 */
 	public function stream_tell() {
 		return ftell( $this->filehandle );
 	}
@@ -41,7 +68,5 @@ class FileWrappingImportStreamHandler {
 	public function stream_close(): bool {
 		return fclose( $this->filehandle );
 	}
-
-	// phpcs:enable
 
 }
